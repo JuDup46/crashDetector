404756e86999d0174bea1543fbf229246836007a
AntoineThiel <antoine.thiel@hotmail.fr>
2022-03-10 10:31:55 +0100

    first comit app

---
+++ app/src/main/java/fr/esgi/crashdetector/fragments/HomeFragment.kt
@@ -0,0 +1,29 @@
+package fr.esgi.crashdetector.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import fr.esgi.crashdetector.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navHost = childFragmentManager.findFragmentById(R.id.home_nav_host) as NavHostFragment
+        NavigationUI.setupWithNavController(home_bottom_nav, navHost.navController)
+    }
+}
