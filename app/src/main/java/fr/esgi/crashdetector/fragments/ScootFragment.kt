d13a039fa0712c3a1c6bf179bb804fcc6cb38a82
AntoineThiel <antoine.thiel@hotmail.fr>
2022-03-10 14:10:02 +0100

    merge

--- app/src/main/java/fr/esgi/crashdetector/fragments/ScootFragment.kt
+++ app/src/main/java/fr/esgi/crashdetector/fragments/ScootFragment.kt
@@ -5,14 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import fr.esgi.crashdetector.R
 
 
-class ScootFragment : Fragment() {
+class ScootFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +20,20 @@ class ScootFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trip, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        val scoots:Array<String> = arrayOf("scoot1", "scoot2");
+
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/fr/esgi/crashdetector/fragments/TripFragment.kt
+++ app/src/main/java/fr/esgi/crashdetector/fragments/TripFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.esgi.crashdetector.R
+import fr.esgi.crashdetector.adapter.CustomAdapter
+import kotlinx.android.synthetic.main.fragment_trip.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class TripFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +25,15 @@ class TripFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trip, container, false)
     }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        view.recycler_trip.layoutManager = LinearLayoutManager(requireContext())
+        view.recycler_trip.adapter = CustomAdapter();
+
+    }
+
+
 }
