f9a9b9ef6f2a5812d84ce2e8169a18a256e8bab0
AntoineThiel <antoine.thiel@hotmail.fr>
2022-03-10 18:44:23 +0100

    final screené

--- app/src/main/java/fr/esgi/crashdetector/fragments/AlertFragment.kt
+++ app/src/main/java/fr/esgi/crashdetector/fragments/AlertFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import fr.esgi.crashdetector.R
 
 class AlertFragment : Fragment() {
@@ -21,9 +23,11 @@ class AlertFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val btn: Button = view.findViewById(R.id.btn_home)
-        btn.setOnClickListener({
+        val imgView: ImageView = view.findViewById(R.id.img_ambu)
+        btn.setOnClickListener {
             val action = AlertFragmentDirections.actionAlertFragmentToRunFragment()
             findNavController().navigate(action)
-        })
+        }
+        Glide.with(this).load("file:///android_asset/pls.gif").into(imgView);
     }
 }
